0cbc332352ee96978b23ea1ab35fb9b2c550450d
Charly <102165285+Charlyy95@users.noreply.github.com>
2025-04-16 23:07:29 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,9 +1,41 @@
+#include "animal.h"
+#include "fillTable.h"
 
-#include "fichier.h"
 
-int main(){
+int main() {
+    Animal shelter[MAX_ANIMALS];
+    int animal_count = 0;
+    int choice;
 
-	printf ("test\n");
-	
-	return 0;
+    printf("=== ChenYI-Tech Animal Shelter ===\n");
+    
+    while (1) {
+        printf("\nMain Menu:\n");
+        printf("1. Add animal\n");
+        printf("2. Search animal\n");
+        printf("4. Exit\n");
+        printf("Choice: ");
+        
+        scanf("%d", &choice);
+        getchar(); // Clear newline
+        
+        switch (choice) {
+            case 1 :
+                if (!addAnimal(shelter, &animal_count)) {
+                    printf("Failed to add animal!\n");
+                }
+                break;
+            case 2:
+                search(fillTable());
+				break;
+			case 3 :
+                //printf("Exiting...\n");
+                break;
+			case 4 :	
+                printf("Exiting...\n");
+                return 0;
+            default:
+                printf("Invalid choice!\n");
+        }
+    }
 }
