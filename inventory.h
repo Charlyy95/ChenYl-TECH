73bcfc2d7c3eefb8773432b25d3fa2f3d153437b
Charly <102165285+Charlyy95@users.noreply.github.com>
2025-05-05 09:55:42 +0200

    Update inventory.h

--- inventory.h
+++ inventory.h
@@ -10,7 +10,7 @@ typedef struct {
 }SpeciesCount;
 
 void speciesInsertionSort(SpeciesCount *tab, int n);
-void ageInsertionSort(int *tab, int *pAnimals);
+void ageInsertionSort(int *tab, int size);
 
 void species_inventory(Animal * tab, int * pAnimals);
 void age_inventory (Animal*tab, int *pAnimals);
