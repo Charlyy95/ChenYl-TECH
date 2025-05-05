37db992bfd8d6b4163c3777e2915b1c9fd9cd6f4
Ilyes1810 <mdk95130@gmail.com>
2025-05-05 09:46:48 +0200

    Update deleteAnimal.c

--- deleteAnimal.c
+++ deleteAnimal.c
@@ -2,23 +2,32 @@
 int askAnimal(Animal *tab, int size) {
     int id;
     int exist = 0;
+   int result;
     do {
         printf("Quel est l'identifiant de l'animal que vous souhaitez retirer ?\n");
-        scanf("%d", &id);
+        result = scanf("%d", &id);
 
-       
-        for (int i = 0; i < size; i++) {
-          if ((tab + i)->id == id)  {
-                exist = 1;
-                break;
-            }
+        while (getchar() != '\n'); // Vider le buffer
+
+        if (result != 1) {
+          printf("Entrée invalide. Veuillez entrer un entier.\n");
+          continue;
+      }
+
+      exist = 0;
+      for (int i = 0; i < size; i++) {
+          if (tab[i].id == id) {
+              exist = 1;
+              break;
         }
+    }
 
-        if (!exist) {
-            printf("Aucun animal avec l'identifiant %d. Réessayez.\n", id);
-        }
+    if (!exist) {
+        printf("Aucun animal avec l'identifiant %d. Réessayez.\n", id);
+    }
 
-    } while (!exist);
+} while (!exist);
+
 
     return id;
 }
@@ -44,29 +53,12 @@ void deleteAnimalByID(Animal *tab, int *size, int idSearch) {
     char nameFile[100];
     sprintf(nameFile, "animaux/%d.txt", idSearch); 
 
+
     if (remove(nameFile) == 0) {
         printf("Fichier '%s' supprimé avec succès.\n", nameFile);
-    } else {
-        printf("Erreur lors de la suppression du fichier '%s'.\n", nameFile);
-    }
+      } 
+    else {
+      perror("Erreur lors de la suppression du fichier");
 }
 
-  
-   /*for (int i = index; i < *size - 1; i++) {
-        *(tab+i) = *(tab+i+1);
-    }
-
-    (*size)--;
-    tab = realloc(tab, (*size) * sizeof(Animal));
-        if (tab == NULL && *size > 0) {
-            perror("Erreur lors du realloc");
-            exit(EXIT_FAILURE);
-    }
-    printf("Animal avec ID %d supprimé du tableau.\n", idSearch);
-    */
-    
-
-
-// dans le main : 
-//int id = askAnimal(animaux, size);
-//supprimerAnimalParID(animaux, &size, id);
+}
